b412a953e92c851e375831b511dbdee06e96b6d9
tdsrmd <demirsedatdemir@icloud.com>
2023-02-25 21:23:29 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 //Patterns
-import Pattern from "./assets/pattern.svg";
-import Pattern2 from "./assets/pattern-02.svg";
-import Pattern3 from "./assets/pattern-03.svg";
-import Pattern4 from "./assets/pattern-04.svg";
+import { Pattern, Pattern2, Pattern3, Pattern4 } from "./assets/patterns";
 
 const App = () => {
   return (
@@ -19,23 +16,30 @@ const App = () => {
             <Content />
             <div className="absolute bottom-0 left-60">
               <img
+                alt="Pattern"
                 src={Pattern2}
                 className=" rounded-tl-3xl rounded-tr-3xl shadow-xl"
               />
             </div>
             <div className="absolute bottom-20 right-20 ">
-              <img src={Pattern3} className="rounded-3xl shadow-xl" />
+              <img
+                alt="Pattern"
+                src={Pattern3}
+                className="rounded-3xl shadow-xl"
+              />
             </div>
           </div>
         </div>
         <div className="absolute top-1/3 left-0 ">
           <img
+            alt="Pattern"
             src={Pattern}
             className=" rounded-tr-2xl rounded-br-2xl shadow-xl"
           />
         </div>
         <div className="absolute top-1/4 right-0 ">
           <img
+            alt="Pattern"
             src={Pattern4}
             className=" rounded-tl-2xl rounded-bl-2xl shadow-xl"
           />
---
+++ src/assets/patterns/index.js
@@ -0,0 +1,6 @@
+import Pattern from "./pattern.svg";
+import Pattern2 from "./pattern-02.svg";
+import Pattern3 from "./pattern-03.svg";
+import Pattern4 from "./pattern-04.svg";
+
+export { Pattern, Pattern2, Pattern3, Pattern4 };
